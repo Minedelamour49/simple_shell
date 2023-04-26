ff19673ef51485eaafc564cafb0bbbfb02786cd2
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-04-26 14:00:08 +0300

    Update get_sigint.c

--- get_sigint.c
+++ get_sigint.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * get_sigint - Handle the crtl + c call in the prompt
+ * get_sigint - Handle the crtl + c call in prompt
  * @sig: Signal handler
  */
 void get_sigint(int sig)
