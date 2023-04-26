14bc95d4b2b83269c0c5534ac842b80b546a0b6d
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-04-26 13:59:09 +0300

    Update get_help.c

--- get_help.c
+++ get_help.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * get_help - function that retrieves help messages according builtin
@@ -9,21 +9,21 @@ int get_help(data_shell *datash)
 {
 
 	if (datash->args[1] == 0)
-		auxiliary_help_general();
+		aux_help_general();
 	else if (_strcmp(datash->args[1], "setenv") == 0)
-		auxiliary_help_setenv();
+		aux_help_setenv();
 	else if (_strcmp(datash->args[1], "env") == 0)
-		auxiliary_help_env();
+		aux_help_env();
 	else if (_strcmp(datash->args[1], "unsetenv") == 0)
-		auxiliary_help_unsetenv();
+		aux_help_unsetenv();
 	else if (_strcmp(datash->args[1], "help") == 0)
-		auxiliary_help();
+		aux_help();
 	else if (_strcmp(datash->args[1], "exit") == 0)
-		auxiliary_help_exit();
+		aux_help_exit();
 	else if (_strcmp(datash->args[1], "cd") == 0)
-		auxiliary_help_cd();
+		aux_help_cd();
 	else if (_strcmp(datash->args[1], "alias") == 0)
-		auxiliary_help_alias();
+		aux_help_alias();
 	else
 		write(STDERR_FILENO, datash->args[0],
 		      _strlen(datash->args[0]));
