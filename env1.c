3fba4e85b38e52b00c865b26c3df8d381291d445
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-04-26 13:56:38 +0300

    Update env1.c

--- env1.c
+++ env1.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * cmp_env_name - compares env variables names
@@ -26,12 +26,12 @@ int cmp_env_name(const char *nenv, const char *name)
 /**
  * _getenv - get an environment variable
  * @name: name of the environment variable
- * @_environment: environment variable
+ * @_environ: environment variable
  *
  * Return: value of the environment variable if is found.
  * In other case, returns NULL.
  */
-char *_getenv(const char *name, char **_environment)
+char *_getenv(const char *name, char **_environ)
 {
 	char *ptr_env;
 	int i, mov;
@@ -39,15 +39,15 @@ char *_getenv(const char *name, char **_environment)
 	/* Initialize ptr_env value */
 	ptr_env = NULL;
 	mov = 0;
-	/* Compare all the environment variables */
-	/* environment is declared in the header file */
-	for (i = 0; _environment[i]; i++)
+	/* Compare all environment variables */
+	/* environ is declared in the header file */
+	for (i = 0; _environ[i]; i++)
 	{
-		/* If the name and env are equal */
-		mov = cmp_env_name(_environment[i], name);
+		/* If name and env are equal */
+		mov = cmp_env_name(_environ[i], name);
 		if (mov)
 		{
-			ptr_env = _environment[i];
+			ptr_env = _environ[i];
 			break;
 		}
 	}
@@ -56,7 +56,7 @@ char *_getenv(const char *name, char **_environment)
 }
 
 /**
- * _env - prints the environment variables
+ * _env - prints the evironment variables
  *
  * @datash: data relevant.
  * Return: 1 on success.
@@ -65,13 +65,13 @@ int _env(data_shell *datash)
 {
 	int i, j;
 
-	for (i = 0; datash->_environment[i]; i++)
+	for (i = 0; datash->_environ[i]; i++)
 	{
 
-		for (j = 0; datash->_environment[i][j]; j++)
+		for (j = 0; datash->_environ[i][j]; j++)
 			;
 
-		write(STDOUT_FILENO, datash->_environment[i], j);
+		write(STDOUT_FILENO, datash->_environ[i], j);
 		write(STDOUT_FILENO, "\n", 1);
 	}
 	datash->status = 0;
